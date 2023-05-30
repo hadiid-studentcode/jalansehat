b0a91ed2968e040574acc068ae70a5f353cae4fc
fiki rabani <fikirbn12@gmail.com>
2023-05-30 22:56:19 +0700

    Commit maps

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -7,8 +11,47 @@ export default function Home() {
       <Head>
         <title>JalanSehat | Home</title>
       </Head>
-      <h1 className={`${styles.textcolor} p-2`}>Hello World</h1>
+      <Container>
+        <Row className='container-row-jumbotron'>
+          <Col sm={6}>
+            <img src="./assets/img/maps.png" alt="" />
+            {/* <Image src='./assets/img/maps.png' alt='maps' width='100' height='100'></Image> */}
+          </Col>
+          <Col sm={6} className='container-colum'>
+            <h2 className='text-name'>SELAMAT DATANG DI JALAN SEHAT</h2>
+            <p className='text-desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dignissimos sed tempora voluptatem culpa dolor! Animi adipisci in illo aperiam illum, sit iste, nisi vitae repudiandae officiis cupiditate laudantium doloribus!</p>
+          </Col>
+        </Row>
 
+        <Row className='container-row'>
+          <Col sm={6} className='card-col'>
+            <p>it amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </Col>
+          <Col sm={6} className='container-colum'>
+            <h1>IMAGE MAPS</h1>
+          </Col>
+        </Row>
+        <Row className='container-name-adventags'>
+          <Col>
+            <h1>Adventages</h1>
+          </Col>
+        </Row>
+        <Row className='container-row-adventags'>
+          <Col sm={6} className='card-col-adventags'>
+            <h3>lorem ipsum</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque aspernatur deserunt eum ut pariatur laboriosam! Quasi ipsam dignissimos, quisquam repudiandae odit nisi culpa vero ut quibusdam! Ipsam et maiores perferendis!</p>
+
+            <h3>lorem ipsum</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque aspernatur deserunt eum ut pariatur laboriosam! Quasi ipsam dignissimos, quisquam repudiandae odit nisi culpa vero ut quibusdam! Ipsam et maiores perferendis!</p>
+
+            <h3>lorem ipsum</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque aspernatur deserunt eum ut pariatur laboriosam! Quasi ipsam dignissimos, quisquam repudiandae odit nisi culpa vero ut quibusdam! Ipsam et maiores perferendis!</p>
+          </Col>
+          <Col sm={6} className='container-colum'>
+            <h1>IMAGE MAPS</h1>
+          </Col>
+        </Row>
+      </Container>
 
     </>
   );
--- src/pages/maps.jsx
+++ src/pages/maps.jsx
@@ -1,5 +1,8 @@
 import Map from '../Components/Map';
 import Head from 'next/head';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Maps() {
   return (
@@ -8,9 +11,30 @@ export default function Maps() {
         <title>JalanSehat | Maps</title>
       </Head>
 
+      <div className='header-map'>
+        <h3>LAPORAN JALAN RUSAK</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum maxime architecto exercitationem porro impedit illo qui, laudantium quo quibusdam provident modi reiciendis ea ab consequatur quae numquam quas expedita sint.</p>
+      </div>
 
-      <Map/>
+      <div className='map-container'>
+        <Map/>
+      </div>
 
+      <Container>
+        <Row>
+          <Col className='mark-contrainer' >
+            <button className='btn-laporan'>Laporkan Jalan Rusak</button>
+            <ul>
+              <li className='icon-red'>Kerusakan Tinggi</li>
+              <li className='icon-yellow'>Kerusakan Sedang</li>
+              <li className='icon-blue'>Sedang Perbaikan</li>
+              <li className='icon-green'>Kondisi Jalan Bagus</li>
+            </ul>
+          </Col>
+        </Row>
+
+
+      </Container>
 
     </>
   );
