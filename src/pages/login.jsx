afb2c6d8c2abbe77eaaf72ecdd2b254e62f705c0
Rhazes <109293519+raisadisutopo@users.noreply.github.com>
2023-06-01 02:49:04 +0700

    penambahan 2 file untuk login dan register

---
+++ src/pages/login.jsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+export default function LoginPage() {
+  return (
+    <Form>
+      <Card className="cardLogin">
+        <Card.Body>
+          <Form.Label className="labelLogin">Form Login</Form.Label>
+          <Form.Group className="emailForm" controlId="formBasicEmail">
+            <Form.Control
+              className="emailInput"
+              type="email"
+              placeholder="Enter email"
+            />
+          </Form.Group>
+          <Form.Group className="passwordForm" controlId="formBasicPassword">
+            <Form.Control
+              className="passwordInput"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button className="buttonLogin" type="submit">
+              Masuk
+            </Button>
+          </Form.Group>
+          <Link className="linkDaftar" href="/register">
+            Belum memiliki akun?
+          </Link>
+        </Card.Body>
+      </Card>
+    </Form>
+  );
+}
