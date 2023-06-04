173e78899347f2ea6c55a89fe46b3177d463554d
Rhazes <raisadisutopo@gmail.com>
2023-06-04 14:07:45 +0700

    commit all file

---
+++ src/pages/register.jsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+export default function RegisterPage() {
+  return (
+    <Form>
+      <Card className="cardLogin">
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body>
+          <Form.Label className="labelLogin">Form Daftar</Form.Label>
+          <Form.Group className="emailForm" controlId="formBasicUsername">
+            <Form.Control
+              className="nameInput"
+              type="string"
+              placeholder="Enter Name"
+            />
+          </Form.Group>
+          <Form.Group className="emailForm" controlId="formBasicNumber">
+            <Form.Control
+              className="numberInput"
+              type="string"
+              placeholder="Enter Number"
+            />
+          </Form.Group>
+          <Form.Group className="emailForm" controlId="formBasicEmail">
+            <Form.Control
+              className="emailInput"
+              type="email"
+              placeholder="Enter email"
+            />
+          </Form.Group>
+          <Form.Group className="passwordForm" controlId="formBasicPassword">
+            <Form.Control
+              className="passwordInput"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button className="buttonLogin" type="submit">
+              Daftar
+            </Button>
+          </Form.Group>
+          <Link className="linkDaftar" href="/login">
+            Sudah memiliki akun?
+          </Link>
+        </Card.Body>
+      </Card>
+    </Form>
+  );
+}
