200cedc3ea9797c662a55444d31d640b7c4e9b7d
Dhiya Annisa <dhiyannisaa@gmail.com>
2023-06-13 21:43:31 +0700

    update tempo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 export default function Home() {
   return (
@@ -45,6 +46,59 @@ export default function Home() {
             </div>
           </Col>
         </Row>
+        <div className="advantage">
+          <Container>
+            <div className="advantage-title">
+              <h2>Keunggulan Jalan Sehat</h2>
+            </div>
+            <Row>
+              <Col lg={6}>
+                <Card className="mt-4 mb-20" style={{padding: '40px 30px', borderRadius: 6, background: '#fff', boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.1)'}}>
+                  <div className="d-flex">
+                    <div><img src="https://cdn0.iconfinder.com/data/icons/black-box/64/tile-sorting-1024.png" style={{width: '80px'}}alt=""/></div>
+                    <div className="adventage-desc">
+                      <h4>Fleksibel</h4>
+                      <p>Akses Jalan Sehat kapanpun dan dimanapun dengan cepat & mudah</p>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+              <Col lg={6}>
+                <Card className="mt-4 mb-20" style={{padding: '40px 30px', borderRadius: 6, background: '#fff', boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.1)'}}>
+                  <div className="d-flex">
+                    <div><img src="https://cdn1.iconfinder.com/data/icons/iconoir-vol-3/24/multi-window-1024.png" style={{width: '80px'}}alt=""/></div>
+                    <div className="adventage-desc">
+                      <h4>Multiplatform</h4>
+                      <p>Anda dapat menjalankan Jalan Sehat di berbagai perangkat Anda</p>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+              <Col className="mb-20" lg={6}>
+                <Card className="mt-4 mb-20" style={{padding: '40px 30px', borderRadius: 6, background: '#fff', boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.1)'}}>
+                  <div className="d-flex">
+                    <div><img src="https://cdn0.iconfinder.com/data/icons/user-interface-2063/24/UI_Essential_icon_expanded_2-52-1024.png" style={{width: '80px'}}alt=""/></div>
+                    <div className="adventage-desc">
+                      <h4>Data Realtime</h4>
+                      <p>Temukan posisi Anda secara langsung dan pilih rute jalan terbaik yang Anda inginkan.</p>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+              <Col lg={6}>
+                <Card className="mt-4 mb-20" style={{padding: '40px 30px', borderRadius: 6, background: '#fff', boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.1)'}}>
+                  <div className="d-flex">
+                    <div><img src="https://cdn4.iconfinder.com/data/icons/standard-free-icons/139/Checkin01-1024.png" style={{width: '80px'}}alt=""/></div>
+                    <div className="adventage-desc">
+                      <h4>Laporkan Kerusakan Jalan</h4>
+                      <p>Informasikan kerusakan jalan dengan mudah dengan fitur <b>Laporkan Jalan Rusak</b>.</p>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
         {/* <div className='p-0 m-0' style={Object.assign({background: 'rgb(196,221,209)', background: 'linear-gradient(360deg, rgba(196,221,209,1) 0%, rgba(233,246,240,1) 0%, rgba(237,245,241,1) 0%, rgba(243,243,243,1) 0%'})}>
           <Row className='container-name-adventags'>
             <Col>
