228ccc03b7be1957da15dfa5f0b4db6935f83b56
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2023-06-15 16:57:35 +0700

    oke

--- src/Components/navbarNavbarComponent-copy-1.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import NavbarComponent from './navbar'
-
-describe('<NavbarComponent />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<NavbarComponent />)
-  })
-})
